c0bbf705e32e9c12db8619c7da8de6d58b0e5e6c
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-11-05 20:43:48 -0300

    add NoteSection

---
+++ components/NoteSection.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function NoteSection({ note }: { note: string }) {
+  const handlePublishNote = async () => {
+    // handle publish note
+  };
+  return (
+    <section className="flex flex-col gap-6 w-full justify-center align-middle">
+      <div className="border border-gray-500 p-8 max-w-3xl w-full h-[50vh] overflow-y-auto mx-auto">
+        <p>{note ? note : "..."}</p>
+      </div>
+      <div className="flex justify-center">
+        <Button
+          className="text-xl px-12 py-6"
+          onClick={handlePublishNote}
+          disabled={!note}
+        >
+          Publish note
+        </Button>
+      </div>
+    </section>
+  );
+}
