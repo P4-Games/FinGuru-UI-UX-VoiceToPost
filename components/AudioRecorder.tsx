e68122bbcca19fa46167d4fd2fc7b2e713622761
Luciano Scaminaci <scaminaci.luciano@gmail.com>
2023-11-14 17:17:37 -0300

    add AudioInput

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, ChangeEvent } from "react";
 import { Button } from "@/components/ui/button";
 import encodeWAV from "audiobuffer-to-wav";
 import { IconLoader2 } from "@tabler/icons-react";
@@ -18,6 +18,7 @@ export default function AudioRecorder({
   const [message, setMessage] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [barWidth, setBarWidth] = useState(0);
+  const [file, setFile] = useState<File | null>(null);
 
   const startProgress = () => {
     let timer = setInterval(() => {
@@ -40,6 +41,11 @@ export default function AudioRecorder({
   useEffect(() => {
     setBarWidth(0);
   }, [loading]);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setFile(e.target.files[0]);
+  };
 
   const handleStartRecording = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -76,67 +82,85 @@ export default function AudioRecorder({
       "Estamos procesando tu audio, esto puede tardar hasta 4 minutos..."
     );
     setLoading(true);
-    let blob = new Blob(audioChunksRef.current, {
-      type: "audio/ogg; codecs=opus",
-    });
-    let reader = new FileReader();
-    reader.onload = function (event) {
-      if (event.target) {
-        let audioData = new Uint8Array(event.target.result as ArrayBuffer);
-        const audioContext = new window.AudioContext();
-        audioContext
-          .decodeAudioData(audioData.buffer)
-          .then(async (audioBuffer) => {
-            let wavArrayBuffer = encodeWAV(audioBuffer);
-            let wavBlob = new Blob([wavArrayBuffer], { type: "audio/wav" });
-
-            if (wavBlob.size > 26214400) {
-              // Check if the size exceeds 25MB
-              setMessage(
-                "The audio file size exceeds the maximum limit of 25MB."
-              );
-              setLoading(false);
-              return;
-            }
-
-            const formData = new FormData();
-            formData.append("file", wavBlob, "recorded-note.wav");
-
-            // Send the formData to your backend
-            try {
-              const response = await fetch(
-                "https://finguru-back-voicetopost-qj44in647a-uc.a.run.app" +
-                  "/convert_audio",
-                {
-                  method: "POST",
-                  body: formData,
-                }
-              );
-              const data = await response.json();
-              setMessage("");
-              setLoading(false);
-
-              console.log(data);
-              if (typeof data != "string") return;
-              let formattedData = data?.startsWith("html")
-                ? data.replace("html", "")
-                : data;
-              formattedData = formattedData.replace(/\n/g, "<br />");
-              callBack(formattedData);
-            } catch (err) {
-              setMessage("Ocurrió un error al procesar el audio");
-              setLoading(false);
-              console.log(err);
-            }
-          });
+    if (file !== null) {
+      if (file.size > 26214400) {
+        // Check if the size exceeds 25MB
+        setMessage("The audio file size exceeds the maximum limit of 25MB.");
+        setLoading(false);
+        return;
       }
-    };
-    reader.readAsArrayBuffer(blob);
+
+      const formData = new FormData();
+      formData.set("file", file);
+      sendForm(formData);
+    } else {
+      let blob = new Blob(audioChunksRef.current, {
+        type: "audio/ogg; codecs=opus",
+      });
+      let reader = new FileReader();
+      reader.onload = function (event) {
+        if (event.target) {
+          let audioData = new Uint8Array(event.target.result as ArrayBuffer);
+          const audioContext = new window.AudioContext();
+          audioContext
+            .decodeAudioData(audioData.buffer)
+            .then(async (audioBuffer) => {
+              let wavArrayBuffer = encodeWAV(audioBuffer);
+              let wavBlob = new Blob([wavArrayBuffer], { type: "audio/wav" });
+
+              if (wavBlob.size > 26214400) {
+                // Check if the size exceeds 25MB
+                setMessage(
+                  "The audio file size exceeds the maximum limit of 25MB."
+                );
+                setLoading(false);
+                return;
+              }
+
+              const formData = new FormData();
+              formData.append("file", wavBlob, "recorded-note.wav");
+              sendForm(formData);
+            });
+        }
+      };
+      reader.readAsArrayBuffer(blob);
+    }
+  };
+
+  const sendForm = async (formData: FormData) => {
+    // Send the formData to the backend
+    console.log(formData.get("file"));
+    try {
+      const response = await fetch(
+        "https://finguru-back-voicetopost-qj44in647a-uc.a.run.app" +
+          "/convert_audio",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const data = await response.json();
+      setMessage("");
+      setLoading(false);
+
+      console.log(data);
+      if (typeof data != "string") return;
+      let formattedData = data?.startsWith("html")
+        ? data.replace("html", "")
+        : data;
+      formattedData = formattedData.replaceAll(/\n/g, "<br/>");
+      callBack(formattedData);
+    } catch (err) {
+      setMessage("Ocurrió un error al procesar el audio");
+      setLoading(false);
+      console.log(err);
+    }
   };
 
   return (
     <section className="flex items-center justify-center w-full">
       <div className="flex flex-col items-center gap-4 m-8">
+        <h2 className="font-medium text-2xl">Grabar audio</h2>
         <div className="flex gap-4">
           {isRecording ? (
             <button
@@ -169,18 +193,40 @@ export default function AudioRecorder({
             </button>
           )}
         </div>
+
         <audio src={audioURL} controls />
+        <div className="my-6 text-center">
+          <label
+            className="block font-medium text-2xl mb-4"
+            htmlFor="file_input"
+          >
+            Subir audio
+          </label>
+          <input
+            className="relative m-0 block w-full min-w-0 flex-auto cursor-pointer rounded-full border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] font-normal leading-[2.15] text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+            aria-describedby="file_input_help"
+            id="file_input"
+            type="file"
+            onChange={handleFileChange}
+          />
+          <p
+            className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+            id="file_input_help"
+          >
+            mp3, mp4, m4a y wav (MAX. 25MB).
+          </p>
+        </div>
         <Button
           className="text-xl px-12 py-6"
           onClick={handleSend}
-          disabled={!audioURL || loading}
+          disabled={!(audioURL || file) || loading}
         >
           {loading ? (
             <div className="animate-spin">
               <IconLoader2 />
             </div>
           ) : (
-            "Enviar audio"
+            "Enviar y procesar"
           )}
         </Button>
         {loading && (
